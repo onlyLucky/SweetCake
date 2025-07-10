61049283f75bbca8f7f693c915ae75eb1803a1cc
Feynman <2901954062@qq.com>
2025-07-10 17:36:05 +0800

    feat: 新增卡牌大陆的模块初始化。

---
+++ fruit/CardLand/scenes/card_item.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+# 升级文字颜色值
+const SexLv = ["#ffb1a4","#b3cdff"]
+const SexBar = ["#ff9281","6e9eff"]
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
