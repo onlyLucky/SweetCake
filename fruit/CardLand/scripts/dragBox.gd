85d6f7d05040895ffe5ff5f8d65cdc84223d2b87
Feynman <2901954062@qq.com>
2025-07-17 16:57:12 +0800

    feat: 调整父节点内拖动 card position计算，边界计算。

--- fruit/CardLand/scripts/dragBox.gd
+++ fruit/CardLand/scripts/dragBox.gd
@@ -1,8 +1,9 @@
 extends Control
 
 var is_dragging := false
-var start_position := Vector2()
-var drag_offset := Vector2.ZERO #点击位置偏移
+var cur_card = null # 当前drag card
+var start_position := Vector2.ZERO # 开始拖动位置坐标
+var cur_card_pos := Vector2.ZERO #当前卡片位置信息
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,23 +23,34 @@ func _card_darg_input(event: InputEvent) -> void:
 	
 	if (event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT) or is_touch_event:
 		if event.is_pressed():
-			drag_offset = get_local_mouse_position()
+			start_position = get_local_mouse_position()
 			is_dragging = true
+			# 检测当前在哪个card 上面
+			for child in get_children():
+				if child.get_rect().has_point(get_local_mouse_position()):
+					child.z_index += 1
+					cur_card = child
+					cur_card_pos = child.position
+					break
 			# 防止事件冒泡
 			get_viewport().set_input_as_handled()
 		elif is_dragging:  # 松开手指/鼠标
 			is_dragging = false
-			#z_index = 0
-			#_check_boundary()
+			cur_card.z_index -= 1
+			cur_card_pos = Vector2.ZERO
+			cur_card._check_boundary()
+			cur_card = null
 			get_viewport().set_input_as_handled()
 	
 	elif (event is InputEventMouseMotion or is_drag_event) and is_dragging:
 		# get_global_mouse_position 获取鼠标/触摸点相对于游戏窗口左上角的全局坐标
 		# get_global_transform().get_scale() 获取控件当前的全局缩放比例
-		#if card_con.get_rect().has_point(get_local_mouse_position()):
-		var target_pos = get_global_mouse_position() - drag_offset * get_global_transform().get_scale()
-		#position = get_parent().get_global_transform().affine_inverse() * target_pos
-		#_check_boundary()
+		var drag_offset = start_position - get_local_mouse_position()
+		var target_pos = cur_card_pos - drag_offset
+		var move_pos = get_global_transform().affine_inverse() * target_pos
+		if cur_card:
+			cur_card.set_move_pos(target_pos)
+			cur_card._check_boundary()
 		print("drag card box")
 			
 		get_viewport().set_input_as_handled()
