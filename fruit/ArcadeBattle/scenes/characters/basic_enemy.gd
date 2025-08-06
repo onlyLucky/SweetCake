acf954cd5fdeb85f4686b5b3cc1a3130c29e9365
Feynman <290154062@qq.com>
2025-08-06 22:32:46 +0800

    feat: 完善新增远程攻击方式。

--- fruit/ArcadeBattle/scenes/characters/basic_enemy.gd
+++ fruit/ArcadeBattle/scenes/characters/basic_enemy.gd
@@ -1,15 +1,23 @@
 class_name BasicEnemy
 extends Character
 
-## 两次攻击之间间隔时间
-@export var duration_between_hits: int
-## 攻击等待时间
-@export var duration_prep_hit: int
+const EDGE_SCREEN_BUFFER := 10
+
+## 近战两次攻击之间间隔时间
+@export var duration_between_melee_attack: int
+## 远程两次攻击之间间隔时间
+@export var duration_between_range_attack: int
+## 近战攻击等待时间
+@export var duration_prep_melee_attack: int
+## 远程攻击等待时间
+@export var duration_prep_range_attack: int
 @export var player: Player
 
 var player_slot: EnemySlot = null
-var time_since_last_hit:=Time.get_ticks_msec()
-var time_since_prep_hit:=Time.get_ticks_msec()
+var time_since_last_melee_attack:=Time.get_ticks_msec()
+var time_since_prep_melee_attack:=Time.get_ticks_msec()
+var time_since_last_range_attack:=Time.get_ticks_msec()
+var time_since_prep_range_attack:=Time.get_ticks_msec()
 
 
 func _ready() -> void:
@@ -18,29 +26,39 @@ func _ready() -> void:
 
 func handle_input()->void:
 	if player != null and can_move():
-		
-		# 判断玩家插槽是否还有空位
-		if player_slot == null:
-			player_slot = player.reserve_slot(self)
-		
-		if player_slot != null:
-			# 向玩家移动
-			# 同一个层级的节点树之下可以使用 position 不在同一层级 可以使用global_position
-			var direction := (player_slot.global_position - global_position).normalized()
-			# 向量相减 取length 是向量的长度
-			if is_player_within_range():
-				velocity = Vector2.ZERO
-				if can_attack():
-					state = State.PREP_ATTACK
-					time_since_prep_hit = Time.get_ticks_msec()
-			else:
-				velocity = direction * speed
+		if can_respawn_knives:
+			goto_range_position()
+		else:
+			goto_melee_position()
+
+# 远程攻击移动
+func goto_range_position()->void:
+	pass
+
+# 近程攻击移动
+func goto_melee_position()->void:
+	# 判断玩家插槽是否还有空位
+	if player_slot == null:
+		player_slot = player.reserve_slot(self)
+	
+	if player_slot != null:
+		# 向玩家移动
+		# 同一个层级的节点树之下可以使用 position 不在同一层级 可以使用global_position
+		var direction := (player_slot.global_position - global_position).normalized()
+		# 向量相减 取length 是向量的长度
+		if is_player_within_range():
+			velocity = Vector2.ZERO
+			if can_attack():
+				state = State.PREP_ATTACK
+				time_since_prep_melee_attack = Time.get_ticks_msec()
+		else:
+			velocity = direction * speed
 
 ## 处理准备攻击工作
 func handle_prep_attack()->void:
-	if state == State.PREP_ATTACK and (Time.get_ticks_msec() - time_since_prep_hit < duration_prep_hit):
+	if state == State.PREP_ATTACK and (Time.get_ticks_msec() - time_since_prep_melee_attack < duration_prep_melee_attack):
 		state = State.ATTACK
-		time_since_last_hit = Time.get_ticks_msec()
+		time_since_last_melee_attack = Time.get_ticks_msec()
 		#随机打乱数组中所有元素的顺序
 		anim_attacks.shuffle()
 
@@ -49,7 +67,12 @@ func is_player_within_range() -> bool:
 	return (player_slot.global_position - global_position).length() < 1
 	
 func can_attack() -> bool:
-	if Time.get_ticks_msec() - time_since_last_hit < duration_between_hits:
+	if Time.get_ticks_msec() - time_since_last_melee_attack < duration_between_melee_attack:
+		return false
+	return super.can_attack()
+
+func can_throw() -> bool:
+	if Time.get_ticks_msec() - time_since_last_range_attack < duration_between_range_attack:
 		return false
 	return super.can_attack()
 
--- fruit/ArcadeBattle/scenes/characters/character.gd
+++ fruit/ArcadeBattle/scenes/characters/character.gd
@@ -6,14 +6,20 @@ const GRAVITY := 600.0
 
 ## 是否能复活
 @export var can_respawn: bool
+## 是否能生成飞刀
+@export var can_respawn_knives: bool
 ## 伤害值
 @export var damage: int
 ## 强力攻击伤害值
 @export var damage_power: int
 ## 倒地时间
 @export var duration_grounded: float
+## 飞刀生成时间
+@export var duration_between_knife_respawn: float
 ## 被强力攻击击飞的速度
 @export var fligth_speed: float
+## 是否带刀
+@export var has_knife: bool
 ## 跳跃强度
 @export var jump_intensity:float
 ## 击退强度
@@ -29,9 +35,12 @@ const GRAVITY := 600.0
 @onready var damage_receiver: DamageReceiver = $DamageReceiver
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 @onready var collateral_damage_emitter: Area2D = $CollateralDamageEmitter
+@onready var knife_sprite: Sprite2D = $KnifeSprite
+@onready var projectile_aim: RayCast2D = $ProjectileAim
+
 
 # fall 下落
-enum State {IDLE, WALK,ATTACK,TAKEOFF,JUMP,LAND,JUMPKICK,HURT,FALL, GROUNDED, DEATH, FLY,PREP_ATTACK }
+enum State {IDLE, WALK,ATTACK,TAKEOFF,JUMP,LAND,JUMPKICK,HURT,FALL, GROUNDED, DEATH, FLY,PREP_ATTACK,THROW }
 
 var anim_attacks := []
 var anim_map := {
@@ -48,6 +57,7 @@ var anim_map := {
 	State.DEATH: "grounded",
 	State.FLY: "fly",
 	State.PREP_ATTACK : "idle",
+	State.THROW : "throw",
 }
 # 攻击连招 下标
 var attack_combo_index := 0
@@ -61,7 +71,10 @@ var height_speed := 0.0
 # 最后一击是否成功
 var is_last_hit_successful := false
 var state = State.IDLE
+# 倒地站起
 var time_since_grounded:=Time.get_ticks_msec()
+# 生成飞刀
+var time_since_knife_dismiss:=Time.get_ticks_msec()
 
 func _ready() -> void:
 	damage_emitter.area_entered.connect(on_emit_damage.bind())
@@ -92,12 +105,15 @@ func _process(_delta: float) -> void:
 	handle_prep_attack()
 	# 处理地面状态
 	handle_grounded()
+	handle_knife_respawn()
 	# 处理死亡处理
 	handle_death(_delta)
 	# 设置朝向
 	set_heading()
 	flip_sprites()
+	knife_sprite.visible = has_knife
 	character_sprite.position = Vector2.UP * height
+	knife_sprite.position = Vector2.UP * height
 	# 设置倒地，死亡状态，碰撞体不触发
 	collision_shape.disabled = is_collsion_disabled()
 	move_and_slide()
@@ -124,6 +140,10 @@ func handle_grounded():
 			state = State.DEATH
 		else:
 			state = State.LAND
+
+func handle_knife_respawn() -> void:
+	if can_respawn_knives and not has_knife and (Time.get_ticks_msec() - time_since_knife_dismiss > duration_between_knife_respawn):
+		has_knife = true
 
 func handle_death(delta: float):
 	if state == State.DEATH and not can_respawn:
@@ -190,6 +210,11 @@ func is_collsion_disabled() -> bool:
 # 重置状态
 func on_action_complete() -> void:
 	state = State.IDLE
+
+func on_throw_complete() -> void:
+	state = State.IDLE
+	has_knife = false
+
 # 起跳切换
 func on_takeoff_complete() -> void:
 	state = State.JUMP
@@ -201,6 +226,9 @@ func on_land_complete() -> void:
 # 角色伤害收信号
 func on_receive_damage(amount: int, direction: Vector2,hit_type: DamageReceiver.HitType) -> void:
 	if can_get_hurt():
+		if has_knife:
+			has_knife = false
+			time_since_knife_dismiss = Time.get_ticks_msec()
 		current_health = clamp(current_health - amount,0, max_health)
 		# 血量为空 被击倒 立马倒地
 		if current_health == 0 or hit_type == DamageReceiver.HitType.KNOCKDOWN:
@@ -242,5 +270,5 @@ func on_wall_hit(_wall: AnimatableBody2D):
 	state = State.FALL
 	height_speed = knockdown_intensity
 	# 碰到墙体反弹回来
-	velocity = -velocity / 1.0
+	velocity = -velocity / 2.0
 	
